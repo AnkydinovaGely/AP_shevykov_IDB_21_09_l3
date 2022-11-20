d5be75d403dea947768f41f381a5171bc24b43ab
AnkydinovaGely <77923273+AnkydinovaGely@users.noreply.github.com>
2022-11-21 00:17:15 +0300

    Update lab_3.cpp

--- lab_3.cpp
+++ lab_3.cpp
@@ -1,1 +1,15 @@
 import re
+
+def main(path):
+    path += '.txt'
+    with open(path, 'r') as html:
+        string = html.readlines()
+        for i in string:
+            out = re.findall('href=\"\S*\"', i)
+            if out != []:
+                print(*out)
+
+#input files:
+# 'test1', 'test2'
+path = 'test2'
+main(path)
